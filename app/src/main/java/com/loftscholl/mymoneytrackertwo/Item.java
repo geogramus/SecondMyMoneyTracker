4a18a6fa280f800684a0a1deaa95858063382316
geogramus <geogram@list.ru>
2017-07-03 04:30:43 +0400

    Made a design for the fab and passed the price

--- app/src/main/java/com/loftscholl/mymoneytrackertwo/Item.java
+++ app/src/main/java/com/loftscholl/mymoneytrackertwo/Item.java
@@ -1,10 +1,12 @@
 package com.loftscholl.mymoneytrackertwo;
 
+import java.io.Serializable;
+
 /**
  * Created by Гео on 26.06.2017.
  */
 
-public class Item {
+public class Item implements Serializable {
     public static final String TYPE_EXPENSE = "expense";
     public static final String TYPE_INCOME = "income";
     final String name;
